7d27eb8e17e3b96656fa8e402aed0bf7a277cb37
Ankit Srivastava <asrivast@gatech.edu>
2015-01-31 17:51:02 -0500

    Implemented hamming function. WIP.

--- hamming.cpp
+++ hamming.cpp
@@ -3,6 +3,22 @@
 
 unsigned int hamming(uint64_t x, uint64_t y)
 {
-    // TODO: calculate the hamming distance and return it
-    return 0;
+    /*
+     * Hamming distance between x and y is calculated
+     * by first obtaining XOR of the two numbers and
+     * then counting the number of 1s in the resulting
+     * number by right shifting 64 times (maximum
+     * possible size) and checking the least significant
+     * bit.
+     *
+     * TODO: Is there a better/more efficient way
+     *       of counting 1s in bit representation of a number?
+     */
+    unsigned int d = 0;
+    uint64_t z = x ^ y;
+    for (unsigned short i = 0; i < 64; ++i) {
+      d += z & 1;
+      z = z >> 1;
+    }
+    return d;
 }
