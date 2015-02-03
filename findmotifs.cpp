157f92f0cc3871dbe21bdb31c25dfc03bc280375
Ankit Srivastava <asrivast@gatech.edu>
2015-02-03 14:18:41 -0500

    Further optimizations in serial implementation. Iterate over d values in reverse order and resize the vector every time.

--- findmotifs.cpp
+++ findmotifs.cpp
@@ -2,9 +2,11 @@
 #include "findmotifs.h"
 #include "hamming.h"
 
+#include <stdexcept>
+
 // Returns all l bit combinations of number with d bits flipped in the binary representation.
 void getcombinations(const unsigned int l, const unsigned int d,
-                     const bits_t number, std::vector<bits_t>& combinations, 
+                     const bits_t number, std::vector<bits_t>::iterator& cit,
                      unsigned int currentidx = 0, unsigned int currentd = 0)
 {
     if (d > 0) {
@@ -16,18 +18,39 @@ void getcombinations(const unsigned int l, const unsigned int d,
         // flip the bit by XOR-ing with appropriate number
         bits_t flipped = number ^ (base << idx);
         if ((currentd + 1) < d) {
-          getcombinations(l, d, flipped, combinations, idx + 1, currentd + 1);
+          getcombinations(l, d, flipped, cit, idx + 1, currentd + 1);
         }
         else {
-          combinations.push_back(flipped);
+          *cit = flipped;
+          ++cit;
         }
       }
     }
     else {
       // There is only one possibility, 0, if d is 0.
-      combinations.push_back(number);
+      *cit = number;
+      ++cit;
     }
 }
+
+// Returns the number of ways in which i items can be chosen from l items.
+uint64_t lChoosei(unsigned int l, unsigned int i)
+{
+    if (i == 0) {
+      return 1;
+    }
+
+    if ((i * 2) > l) {
+      i = l - i;
+    }
+    uint64_t result = l;
+    for (unsigned int j = 2; j <= i; ++j) {
+      result *= (l - j + 1);
+      result /= j;
+    }
+    return result;
+}
+
 
 // implements the sequential findmotifs function
 std::vector<bits_t> findmotifs(unsigned int n, unsigned int l,
@@ -51,11 +74,17 @@ std::vector<bits_t> findmotifs(unsigned int n, unsigned int l,
 
     // Since distance from a particular number can be anything less than d,
     // we iterate over all such possibilities in the following for loop.
-    for (unsigned int i = 0; i <= d; ++i) {
+    std::vector<bits_t> combinations;
+    // Iterate in reverse order so that std::vector doesn't have to reallocate memory.
+    for (int i = d; i >= 0; --i) {
       // Get all l digit numbers which differs from the first input (first input is treated as reference)
       // at exactly i bits.
-      std::vector<bits_t> combinations;
-      getcombinations(l, i, input[0], combinations);
+      combinations.resize(lChoosei(l, i));
+      std::vector<bits_t>::iterator cit = combinations.begin();
+      getcombinations(l, i, input[0], cit);
+      if (cit != combinations.end()) {
+        throw std::runtime_error("Not all combinations were found!");
+      }
       for (std::vector<bits_t>::const_iterator c = combinations.begin(); c != combinations.end(); ++c) {
         unsigned int j = 1;
         // Compare the newly obtained number with every other input number.
