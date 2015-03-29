55e797b8b4556771d0c741da090b1f9077ce3d03
Ankit Srivastava <asrivast@gatech.edu>
2015-03-29 00:45:33 -0400

    Transpose code from Cansu.

--- mpi_jacobi.cpp
+++ mpi_jacobi.cpp
@@ -138,11 +138,6 @@ void gather_vector(const int n, double* local_vector, double* output_vector, MPI
             displs[i] = offset;
             scounts[i] = block_decompose(n, q, i);
             offset += scounts[i];
-            
-//                        std::cout << "=================================================================" << std::endl;
-//                        std::cout << "scounts[" << i << "] is " << scounts[i] << std::endl;
-//                        std::cout << "displs[" << i << "] is " << displs[i] << std::endl;
-//                        std::cout << "=================================================================" << std::endl;
         }
     }
     
@@ -199,10 +194,6 @@ void distribute_matrix(const int n, double* input_matrix, double** local_matrix,
             displs[i] = offset;
             scounts[i] = block_decompose(n, q, i) * n;
             offset += scounts[i];
-//            std::cout << "=================================================================" << std::endl;
-//            std::cout << "scounts[" << i << "] is " << scounts[i] << std::endl;
-//            std::cout << "displs[" << i << "] is " << displs[i] << std::endl;
-//            std::cout << "=================================================================" << std::endl;
         }
     }
 
@@ -281,9 +272,6 @@ void transpose_bcast_vector(const int n, double* col_vector, double* row_vector,
 {
     // TODO
     
-    // vector allocations are not here. didn't test this yet.
-    
-    
     int grid_rank;
     MPI_Comm_rank(comm, &grid_rank);
     int p, q;
@@ -300,22 +288,30 @@ void transpose_bcast_vector(const int n, double* col_vector, double* row_vector,
     col_rank = coords[0];
     row_rank = coords[1];
     MPI_Status status;
-    
+
 //    *  To accomplish this, first, each proccessor (i,0) sends it's local vector
 //    *  to the diagonal processor (i,i). Then the diagonal processor (i,i)
 //    *  broadcasts the message among it's column using a column sub-communicator.
     
-    int scount = block_decompose(n, q, col_rank);
-    if(row_rank == 0 && col_rank != 0) // sender processor, exclude (0,0) it doesn't have to send
+    int rank_root = (grid_rank%q);
+    int scount = block_decompose(n, q, rank_root);
+
+    if(grid_rank == 0)
+        memcpy ( row_vector, col_vector, scount*sizeof(double) );
+    
+    else if(row_rank == 0) // sending processor
     {
         MPI_Send(col_vector, scount, MPI_DOUBLE, col_rank, 0, row_comm);
     }
-    else if(row_rank == col_rank && col_rank != 0)//receiver processor, again exclude (0,0)
+    else if(col_rank == row_rank)//receiving processor
     {
         MPI_Recv(row_vector, scount, MPI_DOUBLE, 0, 0, row_comm, &status);
     }
-    
+
     MPI_Bcast(row_vector, scount, MPI_DOUBLE, row_rank, col_comm);
+
+
+    
 }
 
 
@@ -347,24 +343,34 @@ void mpi_matrix_vector_mult(const int n, double* A,
     // distribute the array onto local processors!
     double* local_A = NULL;
     double* local_x = NULL;
-    // distribute_matrix(n, &A[0], &local_A, comm);
+    distribute_matrix(n, &A[0], &local_A, comm);
     distribute_vector(n, &x[0], &local_x, comm);
     
     
-    //I'm trying to access local_x here:
-    
-    if(grid_rank % q == 0)
-        for(int i = 0; i < block_decompose(n, q, grid_rank/q); i++)
-            std::cout <<"processor: " << grid_rank << " local_x["<< i << "] = " << local_x[i] << std::endl;
-    
-    
+//    int i;
+//    if(grid_rank % q == 0)
+//        for(i = 0; i < block_decompose(n, q, grid_rank/q); i++)
+//            std::cout <<"processor: " << grid_rank << " local_x["<< i << "] = " << local_x[i] << std::endl;
+//
+//    for(i = 0; i < block_decompose_by_dim(n, comm, 0) * block_decompose_by_dim(n, comm, 1); i++)
+//        std::cout <<"processor: " << grid_rank << " local_ARRAY["<< i << "] = " << local_A[i] << std::endl;
     
     // allocate local result space
-    // double* local_y = new double[block_decompose_by_dim(n, comm, 0)];
-    // distributed_matrix_vector_mult(n, local_A, local_x, local_y, comm);
+    double* local_y = new double[block_decompose_by_dim(n, comm, 0)];
+//    distributed_matrix_vector_mult(n, local_A, local_x, local_y, comm);
 
+
+    int rank_root = (grid_rank%q);
+    int scount = block_decompose(n, q, rank_root);
+    double* row_vector = new double[scount];
+    transpose_bcast_vector(n, local_x, row_vector, comm);
+    
+    for(int i = 0; i < scount; i++)
+        std::cout <<"       processor: " << grid_rank << " size: " << scount <<  " y["<< i << "] = " << row_vector[i] << std::endl;
+    
     // gather results back to rank 0
-     //gather_vector(n, local_y, y, comm);
+//    gather_vector(n, local_y, y, comm);
+    
 }
 
 // wraps the distributed jacobi function
