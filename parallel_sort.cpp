46e3613c01fcbeac3e67b22c072ab293b6ea1733
r4d2 <patrick.flick@gmail.com>
2015-03-31 19:17:27 -0400

    initial commit of framework

---
+++ parallel_sort.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file    parallel_sort.cpp
+ * @author  Patrick Flick <patrick.flick@gmail.com>
+ * @brief   Implements the parallel, distributed sorting function.
+ *
+ * Copyright (c) 2014 Georgia Institute of Technology. All Rights Reserved.
+ */
+
+#include "parallel_sort.h"
+
+// my shit
+#include <vector>
+#include "io.h"
+
+// implementation of your parallel sorting
+void parallel_sort(int * begin, int* end, MPI_Comm comm) {
+}
+
+
+/*********************************************************************
+ *             Implement your own helper functions here:             *
+ *********************************************************************/
+
+// ...
